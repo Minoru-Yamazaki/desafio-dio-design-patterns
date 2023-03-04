195edd38aa5be672f03014e4907e55b46101da62
Edson Minoru Yamazaki <edson.minoru@muralis.com.br>
2023-03-03 21:41:29 -0300

    Adicionado models

---
+++ src/main/java/br/com/dio/model/Cliente.java
@@ -0,0 +1,20 @@
+package br.com.dio.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    @ManyToOne
+    private Endereco endereco;
+
+}
